66374964f17dd46a8660162569f48e3b7ae60a2b
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2025-11-04 07:40:52 -0500

    Glint 2 + ember-source Upgrade
    Glint 2 + ember-source Upgrade

--- ui/types/global.d.ts
+++
@@ -1,7 +0,0 @@
-// Types for compiled templates
-declare module 'ui/templates/*' {
-  import type { TemplateFactory } from 'ember-cli-htmlbars';
-
-  const tmpl: TemplateFactory;
-  export default tmpl;
-}
