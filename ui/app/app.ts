37999442e05327af2efd1d6f0c3c5d872fcb8d83
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2025-11-04 07:19:21 -0500

    pnpm dlx ember-vite-codemod --skip-git --skip-v2-addon

--- ui/app/app.ts
+++ ui/app/app.ts
@@ -5,12 +5,14 @@ import Application from '@ember/application';
 
 import loadInitializers from 'ember-load-initializers';
 import Resolver from 'ember-resolver';
-import config from 'ui/config/environment';
+import config from "./config/environment";
+
+import compatModules from "@embroider/virtual/compat-modules";
 
 export default class App extends Application {
   modulePrefix = config.modulePrefix;
   podModulePrefix = config.podModulePrefix;
-  Resolver = Resolver;
+  Resolver = Resolver.withModules(compatModules);
 }
 
-loadInitializers(App, config.modulePrefix);
+loadInitializers(App, config.modulePrefix, compatModules);
--- ui/ember-cli-build.js
+++ ui/ember-cli-build.js
@@ -1,33 +1,25 @@
-'use strict';
-
+'use strict';;
 const path = require('path');
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 const stew = require('broccoli-stew');
 const fse = require('fs-extra');
 
+const {
+  compatBuild
+} = require("@embroider/compat");
+
 function isProduction() {
   return EmberApp.env() === 'production';
 }
 
-module.exports = function (defaults) {
+module.exports = async function(defaults) {
+  const {
+    buildOnce
+  } = await import("@embroider/vite");
+
   let app = new EmberApp(defaults, {
     // Add options here
   });
-
-  // Use `app.import` to add additional libraries to the generated
-  // output files.
-  //
-  // If you need to use different assets in different
-  // environments, specify an object as the first parameter. That
-  // object's keys should be the environment name and the values
-  // should be the asset to use in that environment.
-  //
-  // If the library that you are including contains AMD or ES6
-  // modules that you would like to import into your application
-  // please specify an object with the list of modules as keys
-  // along with the exports of each module as its value.
-
-  const { Webpack } = require('@embroider/webpack');
 
   const rootTree = require('@embroider/compat').compatBuild(app, Webpack, {
     staticAddonTestSupportTrees: true,
@@ -96,10 +88,8 @@ module.exports = function (defaults) {
 
   if (process.argv.some((arg) => arg === 'test')) {
     // For tests, we can skip the afterBuild part
-    return rootTree;
+    return compatBuild(app, buildOnce);
   }
 
-  return stew.afterBuild(rootTree, () => {
-    fse.copySync(dist, target, { overwrite: true });
-  });
+  return compatBuild(app, buildOnce);
 };
